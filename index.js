7bc8a301b63c9e50e47979bce8b23bcfde9eb06f
CHRISTOPH TRAUTWEIN <c.trautwein@accenture.com>
2023-04-21 10:19:22 +0200

    add christoph

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World!");
+    return res.send("Hello Christops's Azure World!");
 })
 
 app.get('/api', function (req, res) {
